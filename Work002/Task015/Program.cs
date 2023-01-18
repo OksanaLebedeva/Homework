296fbba3fbe3a78d50ea2169168d9a4c900a21ad
OksanaLebedeva <122013217+OksanaLebedeva@users.noreply.github.com>
2023-01-18 17:05:01 +0300

    Task 15

---
+++ Work002/Task015/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Please enter the number of the weekday: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if ((num<1)||(num>7))
+{
+  Console.WriteLine("There is no such weekday");
+}
+else if ((num==6)||(num==7))
+{
+  Console.WriteLine("It is a WEEKEND!");
+}
+else
+{
+  Console.WriteLine("Still it is a working week");
+}
