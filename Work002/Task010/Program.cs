0545ff1ea64fff97a6c27cc4426d13817f7211e3
OksanaLebedeva <122013217+OksanaLebedeva@users.noreply.github.com>
2023-01-18 16:53:09 +0300

    Task 10

---
+++ Work002/Task010/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Please enter the 3-digit number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if ((Math.Abs(num) < 100))
+{
+  Console.WriteLine("There are less then 3 digits in the number");
+}
+else if (Math.Abs(num)>999)
+{
+  Console.WriteLine("There are more then 3 digits in the number");
+}
+else
+{
+  int result = (Math.Abs(num) / 10) % 10;
+  Console.WriteLine($"Result is {result}");
+}
