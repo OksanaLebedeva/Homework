3dc77b0d1e83dfdf6a1f8b2beffd910c6f6745bc
OksanaLebedeva <122013217+OksanaLebedeva@users.noreply.github.com>
2023-01-21 16:03:54 +0300

    Task 25

---
+++ Work004/Task25/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите цикл, который принимает на вход два числа
+// (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+double Stepen(int a, int b)
+{
+  double result = Math.Pow(a,b);
+  return result;
+}
+Console.Clear();
+Console.Write("Please enter number A: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Please enter number B: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Number '{numA}' in degree of '{numB}' will be {Stepen(numA, numB)}");
