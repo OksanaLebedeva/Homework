2287ad2475721ee6ea6f9a7a37b00640f8746d4d
OksanaLebedeva <122013217+OksanaLebedeva@users.noreply.github.com>
2023-01-23 07:11:42 +0300

    Task 34

---
+++ Work005/Task34/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+void GenerateArray(int[] arr)
+{
+  for (int i=0; i<arr.Length; i++)
+  {
+    arr[i] = new Random().Next(99,1000);
+  }
+}
+void PrintArray(int[] array)
+{
+  Console.Write($"Our array: [");
+  for (int i=0; i<array.Length; i++)
+  {
+    Console.Write($"{array[i]}");
+    if (i<array.Length-1) Console.Write(", ");
+    else Console.Write("");
+  }
+  Console.WriteLine("]");
+}
+int CountEven(int[] array)
+{
+  int count = 0;
+  for (int i=0; i<array.Length; i++)
+  {
+    if (array[i]%2==0) count++;
+  }
+  return count;
+}
+int[] ourarray = new int[10];
+GenerateArray(ourarray);
+PrintArray(ourarray);
+Console.WriteLine($"Count of even elements is: {CountEven(ourarray)}");
