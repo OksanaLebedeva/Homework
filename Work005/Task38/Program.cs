7709b3916b973c8ef5c969b9ced9edeffb196ba3
OksanaLebedeva <122013217+OksanaLebedeva@users.noreply.github.com>
2023-01-23 08:27:51 +0300

    Task 38 UPD

--- Work005/Task38/Program.cs
+++ Work005/Task38/Program.cs
@@ -1,14 +1,14 @@
 ﻿// Задайте массив вещественных чисел. Найдите разницу
 // между максимальным и минимальным элементов массива.
 // [3 7 22 2 78] -> 76
-void GenerateArray(int[] arr)
+void GenerateArray(double[] arr)
 {
   for (int i=0; i<arr.Length; i++)
   {
-    arr[i] = new Random().Next(-19,20);
+    arr[i] = Math.Round(new Random().Next(-19,20) + new Random().NextDouble(),2);
   }
 }
-void PrintArray(int[] array)
+void PrintArray(double[] array)
 {
   Console.Write($"Our array: [");
   for (int i=0; i<array.Length; i++)
@@ -19,25 +19,25 @@ void PrintArray(int[] array)
   }
   Console.WriteLine("]");
 }
-int FindMax (int[] array)
+double FindMax (double[] array)
 {
-  int maxElement = array[0];
+  double maxElement = array[0];
   for (int i=1; i<array.Length; i++)
   {
     if (array[i]>maxElement) maxElement = array[i];
   }
   return maxElement;
 }
-int FindMin (int[] array)
+double FindMin (double[] array)
 {
-  int minElement = array[0];
+  double minElement = array[0];
   for (int i=1; i<array.Length; i++)
   {
     if (array[i]<minElement) minElement = array[i];
   }
   return minElement;
 }
-int[] ourArray = new int[10];
+double[] ourArray = new double[10];
 GenerateArray(ourArray);
 PrintArray(ourArray);
 Console.WriteLine($"Difference between MAX element '{FindMax(ourArray)}' "
