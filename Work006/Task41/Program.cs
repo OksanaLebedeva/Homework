eb9bdd7bec253d9f91bbb34d57c21a712bc2e0f7
OksanaLebedeva <122013217+OksanaLebedeva@users.noreply.github.com>
2023-01-24 14:05:17 +0300

    Task 41 upd

--- Work006/Task41/Program.cs
+++ Work006/Task41/Program.cs
@@ -2,6 +2,8 @@
 // сколько чисел больше 0 ввёл пользователь.
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
+
+// First option - array is entered by user
 Console.Write("How many numbers? ");
 int num = Convert.ToInt32(Console.ReadLine());
 int count = 0;
@@ -13,3 +15,26 @@ for (int i=0; i<num; i++)
   if (arr[i]>0) count++;
 }
 Console.WriteLine($"There are {count} positive numbers");
+
+// Second option - array was pre-entered
+void PrintArray(int[] array)
+{
+  for (int i=0; i<array.Length; i++)
+  {
+    Console.Write($"{array[i]}");
+    if (i<array.Length-1) Console.Write(", ");
+    else Console.Write("");
+  }
+  Console.WriteLine("]");
+}
+int[] arr2 = {-1, 0, 5, 7, -10, 18, -34, 55};
+Console.WriteLine();
+Console.Write("Initial array: [");
+PrintArray(arr2);
+
+int count2 = 0;
+for (int i=0; i<arr2.Length; i++)
+{
+  if (arr2[i]>0) count2++;
+}
+Console.WriteLine($"There are {count2} positive numbers");
