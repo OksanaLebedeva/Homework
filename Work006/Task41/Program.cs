9fc222da84c9a230d94db53aa59680e181301499
OksanaLebedeva <122013217+OksanaLebedeva@users.noreply.github.com>
2023-01-24 13:53:29 +0300

    Task 41

---
+++ Work006/Task41/Program.cs
@@ -0,0 +1,15 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте,
+// сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+Console.Write("How many numbers? ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+int[] arr = new int[num];
+for (int i=0; i<num; i++)
+{
+  Console.Write($"Please enter {i+1} number: ");
+  arr[i] = Convert.ToInt32(Console.ReadLine());
+  if (arr[i]<0) count++;
+}
+Console.WriteLine($"There are {count} negative numbers");
