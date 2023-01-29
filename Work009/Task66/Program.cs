b10c2db46eaee1bf451b2cf7e823eb7c88c1609d
OksanaLebedeva <122013217+OksanaLebedeva@users.noreply.github.com>
2023-01-29 17:57:43 +0300

    Task 66

---
+++ Work009/Task66/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте значения M и N. Напишите программу, которая
+// найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+int SumBetweenNumbers (int a, int b)
+{
+    if (a == b) return b;
+    else return a+SumBetweenNumbers(a+1, b);
+} 
+Console.Write("Please enter 1st number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Please enter 2nd number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Sum of numbers between {num1} and {num2} = '{SumBetweenNumbers(num1, num2)}'");
