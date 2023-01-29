a53615eb12052ad1c24eb9b9d136bc1712bc3f11
OksanaLebedeva <122013217+OksanaLebedeva@users.noreply.github.com>
2023-01-29 17:48:55 +0300

    Task 64

---
+++ Work009/Task64/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все
+// натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+string PrintNumber (int a)
+{
+    if (a == 1) return "1";
+    else return $"{a}, " + $"{PrintNumber(a-1)}";
+} 
+Console.Write("Please enter number: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Numbers between {x} and 1 = '{PrintNumber(x)}'");
